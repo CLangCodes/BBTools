d3ce5fc2cb67b0aa8c67422175157236f030757b
Misfit Initiative <104792124+Arktoh@users.noreply.github.com>
2025-05-30 17:05:34 -0400

    More cleanup

--- BBTools/Models/AntigenSelection.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BBTools.Models
-{
-    public class AntigenSelection
-    {
-        public string Name { get; set; } = string.Empty;
-        public decimal Frequency { get; set; }
-        public bool IsSelected { get; set; } = false;
-    }
-}
